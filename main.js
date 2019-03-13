79694914a5d9716887bf1f98767ea81bf34572a8
Derek <derekdenhartigh@gmail.com>
2019-03-13 18:46:16 -0400

    doing some form styling, still need to figure out adding/removing classes of the circles after being clicked.

--- main.js
+++ main.js
@@ -3,12 +3,13 @@
 What does the application do?
     1. Displays the restaurant’s floor plan.
 2. Visually notifies the user that seats are available or not.
-3. Displays a form when an open table is clicked on.
-4. Hides the form when the user submits the form.
+    3. Displays a form when an open table is clicked on.
+    4. Hides the form when the user submits the form.
+
 Build Specifications:
-1. Each table must have either an “available” or a “reserved” class.
-2. The form must remain hidden until a user clicks on a table with the class “available”.
-3. There should be a way for the user to exit the form without submitting.
+    1. Each table must have either an “available” or a “reserved” class.
+    2. The form must remain hidden until a user clicks on a table with the class “available”.
+    3. There should be a way for the user to exit the form without submitting.
 4. When the user submits the form, the form is hidden, and the class of the selected seat
 is changed from “available” to “reserved”.
 5. Change the cursor property to “not-allowed” if the table has a “reserved” class.
@@ -20,16 +21,60 @@ $(document).ready(function() {
 
     $(".circle").hover(
         function(){
-            $(this).css('background-color', ' #ADAAAA');
-            $(this).css('color', '#CCC');  // styles on hover
-    },   function(){
-        $(this).css('background-color', '#CCC');
-        $(this).css('color', '#ADAAAA'); // styles after hover
-    });
+            $(this).css('border', 'solid #008000 2px');
+            if($(this).hasClass('reserved')){
+                $(this).css('cursor', 'not-allowed');
+            }
+        },   
+        function(){
+            $(this).css('border', 'none');
+        }
+    );  // for UX
+
+// console.log($('.circle').hasClass('reserved')) // returns boolean true/false
 
     $(".circle").on('click', ()=>{
-        
-    })
+        if($('.circle').hasClass('available')===true){
+            $(this).removeClass('available');
+            $(this).addClass('reserved');
+            $('#form').removeClass('hidden');
+            $('#form').addClass('revealed');
+            return;
+        }
+        else {alert("something aint working here!")};
+    });
+
+    $('#x').hover(
+        function(){
+            $(this).css("cursor", "pointer");
+            $(this).css("color", "black");
+        },  
+        function(){
+            $(this).css("cursor", 'default');
+            $(this).css("color", "#ADAAAA");
+        });
+
+    $('#save-button').hover(
+        function(){
+            $(this).css("cursor", "pointer");
+            $(this).css("color", "black");
+            $(this).css('border', "solid black 2px");
+        },  
+        function(){
+            $(this).css("cursor", 'default');
+            $(this).css("color", "white");
+            $(this).css("border", "none");
+        });
+
+    $("#x").on('click', ()=>{
+            $('#form').removeClass('revealed');
+            $('#form').addClass('hidden');
+        });
+
+    $("#save-button").on('click', ()=>{
+        $('#form').removeClass('revealed');
+        $('#form').addClass('hidden');
+    });
 });
 
 /*
